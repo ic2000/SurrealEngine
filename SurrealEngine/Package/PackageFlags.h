9d70f270c4460fef26061884a1dd44f295048a75
Ibrahim Cengiz <ic2000@live.co.uk>
2023-07-03 02:06:48 +0100

    Add missing includes to compile on Clang 15

--- SurrealEngine/Package/PackageFlags.h
+++ SurrealEngine/Package/PackageFlags.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 enum class PackageFlags : uint32_t
 {
 	NoFlags = 0,
--- SurrealEngine/Window/Window.h
+++ SurrealEngine/Window/Window.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Math/vec.h"
+#include <memory>
 
 class Engine;
 class RenderDevice;
